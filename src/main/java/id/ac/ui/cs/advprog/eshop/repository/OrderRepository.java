04b5e335012a6ecc663ecb8ab01816f3b817b218
nadriha <hafez.nadhira@gmail.com>
2024-03-05 20:38:17 +0700

    [RED] Add tests for OrderRepository.

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+
+public class OrderRepository {
+    public Order save(Order order){
+
+    }
+
+    public Order findById(String id){
+
+    }
+}
