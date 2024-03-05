fc68b7c687b8ddbd149497ccf2bb664a4bebab9a
nadriha <hafez.nadhira@gmail.com>
2024-03-05 21:10:32 +0700

    [RED] Add OrderRepository skeleton.

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -1,13 +1,24 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import id.ac.ui.cs.advprog.eshop.model.Order;
 
 public class OrderRepository {
-    public Order save(Order order){
+    public List<Order> orderData = new ArrayList<>();
 
+
+    public Order save(Order order){
+        return null;
     }
 
     public Order findById(String id){
-
+        return null;
     }
+
+    public List<Order> findAllByAuthor(String author){
+        return null;
+    }
+
 }
